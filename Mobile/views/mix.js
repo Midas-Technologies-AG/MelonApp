9123503764f069c904124910428f26cc5b0df647
Kunal Sachdeva <kunsachdeva@gmail.com>
2019-10-07 15:50:21 +0530

    remove redundancy in asset rendering

--- Mobile/views/mix.js
+++ Mobile/views/mix.js
@@ -10,20 +10,14 @@ export default class Mix extends Component {
   async componentDidMount() {
     var assets = await getAllAssets();
     var holdings = await getHoldings()
-    // console.warn(holdings);
-    // console.warn(assets)
     holdings.forEach(holding => {
-      console.warn(holding);
       var assetIndex = assets.findIndex(asset => asset.token.address === holding.token.address);
       assets[assetIndex] = Object.assign({}, assets[assetIndex], { quantity: holding.quantity / Math.pow(10, holding.token.decimals) })
     });
-    // assets = assets.map(asset => Object.assign({}, asset, ))
     this.setState((prevState, props) => Object.assign({}, prevState, { assets, isLoading: false }))
   }
 
   render() {
-    console.warn(this.state.assets);
-
     return <ScrollView>
       <Title text='Fund' />
       {(this.state.isLoading) ? <Text style={{ margin: 16 }}>Loading...</Text> : this.renderAssets()}
@@ -38,10 +32,14 @@ export default class Mix extends Component {
   }
 
   renderAssetsWithBalance() {
-    return this.state.assets.filter(asset => !!asset.quantity).map(asset => <Asset key={asset.token.symbol} name={asset.token.name} symbol={asset.token.symbol} balance={asset.quantity || 0} />)
+    return this.state.assets.filter(asset => asset.quantity).map(this.renderAsset)
   }
 
   renderAssetsWithoutBalance() {
-    return this.state.assets.filter(asset => !asset.quantity).map(asset => <Asset key={asset.token.symbol} name={asset.token.name} symbol={asset.token.symbol} balance={asset.quantity || 0} />)
+    return this.state.assets.filter(asset => !asset.quantity).map(this.renderAsset)
+  }
+
+  renderAsset(asset){
+    return <Asset key={asset.token.symbol} name={asset.token.name} symbol={asset.token.symbol} balance={asset.quantity || 0} />
   }
 }
