2fa63be49057b924adfb2677b2402630a01fe9c7
Kunal Sachdeva <kunsachdeva@gmail.com>
2019-10-21 12:49:23 +0530

    add basic structure

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,10 +1,97 @@
 import React from 'react';
+import './styles/main.css'
+import './styles/header.css'
+import PropTypes from 'prop-types';
+import { getAllAssets, getEnvironment } from './wrapper/melon'
 
-function App() {
-  return (
-    <div>
+var getImageUrl = (symbol) => {
+  if (symbol == 'DGX') symbol = 'DGD';
+  if (symbol == 'WETH') symbol = 'ETH';
+  return 'https://raw.githubusercontent.com/atomiclabs/cryptocurrency-icons/master/svg/black/' + symbol.toLowerCase() + '.svg?sanitize=true'
+};
+
+class App extends React.Component {
+
+  constructor(props, context) {
+    super(props)
+    this.state = { assets: new Array(), selectedIndex: 0 }
+    const web3Context = context.web3;
+    console.warn(web3Context);
+  }
+
+  async componentDidMount() {
+    // var assets = await getAllAssets();
+    var env = await getEnvironment();
+    console.warn(env);
+    
+    var assets = [{ "token": { "address": "0xB14c0f4a8150c028806bE46Afb5214daea870CB7", "name": "Basic Attention Token", "symbol": "BAT", "decimals": 18 } }, { "token": { "address": "0x16886a2B35BF40C59087500dEC9Bbc24765382C2", "name": "Digix Gold Token", "symbol": "DGX", "decimals": 9 } }, { "token": { "address": "0xa80C98433E2a82DF3636ED934083E3285163Fad8", "name": "Rep Token", "symbol": "REP", "decimals": 18 } }, { "token": { "address": "0x0A3610a0E87cEDDEE6b81b62b462c7a0fD450E2a", "name": "ZeroX Protocol Token", "symbol": "ZRX", "decimals": 18 } }, { "token": { "address": "0xd0a1e359811322d97991e03f863a0c30c2cf029c", "name": "Eth Token", "symbol": "WETH", "decimals": 18 } }, { "token": { "address": "0x2C2edf394638931eb672BD9261d2AA1934874d45", "name": "Melon Token", "symbol": "MLN", "decimals": 18 } }, { "token": { "address": "0xbdaD7a926A7E70C6B0AF367d97D992b904BBAFcf", "name": "MakerDao", "symbol": "MKR", "decimals": 18 } }, { "token": { "address": "0x1D3bC44DD6C3F00640A6825B48F1C78770fd21d8", "name": "Dai", "symbol": "DAI", "decimals": 18 } }, { "token": { "address": "0xB5098BAFbF90F278374EcFA973A703fD0eb87A12", "name": "Kyber Network", "symbol": "KNC", "decimals": 18 } }]
+    this.setState((prevState, props) => Object.assign({}, prevState, { assets }))
+  }
+
+  render() {
+    var selectedAsset = this.state.assets[this.state.selectedIndex]
+    console.warn(window.web3);
+    return (
+      <div>
+        <header>
+          <span className="fund-name">KUNIMIX</span>
+          <span className="share-price"><b>SHARE PRICE</b> 0.91123</span>
+        </header>
+        <main>
+          <div className="fund-view">
+            {this.state.assets.map((asset, i) => (<div className={i == this.state.selectedIndex ? "asset selected" : "asset"} onClick={_ => this.selectAsset(i)}>
+              <img src={getImageUrl(asset.token.symbol)} height="40" width="40" />
+              <span className="title">{asset.token.symbol}</span>
+              <br />
+              <div className="subtitle">{asset.token.name}</div>
+              <div className="balance">123.34</div>
+            </div>))}
+          </div>
+          {(!selectedAsset) ? null : <div className="asset-view">
+            <h1>{selectedAsset.token.name}</h1>
+            <h3>BALANCE <span style={{ fontWeight: '100' }}>{123.339893423}</span></h3>
+            <div className="input-container">
+              {this.renderInput('WETH')}
+              {this.renderInput(selectedAsset.token.symbol)}
+            </div>
+            {this.renderBuySellButton()}
+            <h2>ORDERBOOK</h2>
+            <div className="orders">
+              {this.renderOrder('add')}
+              {this.renderOrder('remove')}
+            </div>
+          </div>}
+        </main>
+      </div>
+    );
+  }
+
+  selectAsset(selectedIndex) {
+    this.setState((prevState, props) => Object.assign({}, prevState, { selectedIndex }))
+  }
+
+  renderInput(asset) {
+    return <input type="number" placeholder={asset} className="input-value" />
+  }
+  renderBuySellButton() {
+    return <div style={{ display: 'flex', justifyContent: 'center' }}>
+      {this.renderButton('SELL')}
+      {this.renderButton('BUY')}
     </div>
-  );
+  }
+  renderButton(text) {
+    return <button style={{ backgroundColor: text == 'SELL' ? 'white' : 'black', color: text == 'SELL' ? 'black' : 'white' }}>{text}</button>
+  }
+  renderOrder(type) {
+    return <div className={"order " + type}>
+      <img src={'https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/white/svg/' + (type == 'add' ? 'plus' : 'minus') + '.svg?sanitize=true'} style={{ backgroundColor: type == 'add' ? 'green' : '#e00000' }} />
+      <span>12.4323 MLN for 90.87432 Ξ total</span>
+    </div>
+  }
 }
+
+App.contextTypes = {
+  web3: PropTypes.object
+};
 
 export default App;
---
+++ Web/src/wrapper/melon.js
@@ -0,0 +1,104 @@
+import { INFURA_KEY } from '../env'
+// var Protocol = require('@melonproject/protocol')
+// var takeOasisDexOrder = require('@melonproject/protocol/lib/contracts/fund/trading/transactions/takeOasisDexOrder').takeOasisDexOrder
+// var withPrivateKeySigner = require('@melonproject/protocol/lib/utils/environment/withPrivateKeySigner').withPrivateKeySigner
+var withDeployment = require('@melonproject/protocol/lib/utils/environment/withDeployment').withDeployment
+var constructEnvironment = require('@melonproject/protocol/lib/utils/environment/constructEnvironment').constructEnvironment
+// var exchangeAggregate = require('@melonproject/exchange-aggregator')
+// var createQuantity = require('@melonproject/token-math').createQuantity
+
+const ENDPOINT = 'https://kovan.infura.io/v3/' + INFURA_KEY;
+// const EXCHANGES = ['oasisdex']
+
+// var generateExchangeAggregatorOptions = async (baseSymbol = 'WETH', quoteSymbol = 'MLN', environment, network = exchangeAggregate.Network.KOVAN) => {
+//   var manager = environment || await getEnvironment()
+//   return ({
+//     network,
+//     base: baseSymbol,
+//     quote: quoteSymbol,
+//     environment: manager
+//   })
+// }
+
+// export var getOrders = async (baseSymbol, quoteSymbol, action) => {
+//   try {
+//     var exchanges = EXCHANGES;
+//     var manager = await getEnvironment();
+//     var options = await generateExchangeAggregatorOptions(baseSymbol, quoteSymbol, manager)
+//     var orders = new Array();
+//     try {
+//       orders = orders.concat(await exchangeAggregate.exchanges[exchanges[0]].fetch(options))
+//     } catch (e) {
+//       console.warn(e.message);
+//     }
+//     orders = removeDuplicateOrders(orders);
+//     var filteredOrders = orders.filter(o => o.type == 'BID');
+//     (action == 'add') ? filteredOrders.sort((a, b) => Number(a.trade.base.quantity) - Number(b.trade.base.quantity)) : filteredOrders.sort((a, b) => Number(b.trade.base.quantity) - Number(a.trade.base.quantity))
+//     return filteredOrders;
+//   } catch (e) {
+//     return [];
+//   }
+// }
+
+// var getFundData = spoke => JSON.parse(localStorage.getItem('fund'))[spoke];
+// export var getPrivateKey = (mnemonic) => '0x' + (new HDWalletProvider(mnemonic, ENDPOINT)).wallet._privKey.toString('hex')
+// export var getManagerFromPrivateKey = async (privateKey) => await withPrivateKeySigner(await getEnvironment(), privateKey)
+// var getManagerFromLocalStorage = async () => await getManagerFromPrivateKey(localStorage.getItem('privateKey'));
+// export var getManagerFromMnemonic = async (mnemonic) => await getManagerFromPrivateKey(getPrivateKey(mnemonic));
+export var getEnvironment = () => withDeployment(constructEnvironment({ endpoint: ENDPOINT, track: 'kyberPrice' }))
+export var getAllAssets = async () => (await getEnvironment()).deployment.thirdPartyContracts.tokens.map(asset => ({ token: asset }))
+
+// export var getHoldings = async () => {
+//   try {
+//     var accountingAddress = await getFundData('accounting')
+//     var manager = await getEnvironment()
+//     var holdings = await Protocol.getFundHoldings(manager, accountingAddress);
+//     return holdings;
+//   } catch (e) {
+//     alert('Could not load assets: ' + e.message)
+//   }
+// }
+
+// export var getInfo = async () => {
+//   var accountingAddress = await getFundData('accounting')
+//   var environment = await getEnvironment();
+//   return await Protocol.performCalculations(environment, accountingAddress)
+// }
+
+// export var makeOrder = async (quoteSymbol, quantityInWeth, quantityInQuoteToken, action) => {
+//   var tradingAddress = await getFundData('trading')
+//   var manager = await getManagerFromLocalStorage();
+//   const base = Protocol.getTokenBySymbol(manager, 'WETH');
+//   const quote = Protocol.getTokenBySymbol(manager, quoteSymbol);
+//   var makerQuantity = (action == 'BUY') ? createQuantity(base, quantityInWeth) : createQuantity(quote, quantityInQuoteToken);
+//   var takerQuantity = (action == 'BUY') ? createQuantity(quote, quantityInQuoteToken) : createQuantity(base, quantityInWeth);
+//   return await Protocol.makeOasisDexOrder(manager, tradingAddress, { makerQuantity, takerQuantity });
+// }
+
+// export var takeOrder = async (orderId) => {
+//   var tradingAddress = await getFundData('trading')
+//   var manager = await getManagerFromLocalStorage();
+//   var enhancedOrder = await Protocol.getOasisDexOrder(manager, manager.deployment.exchangeConfigs.MatchingMarket.exchange, { id: orderId });
+//   return await takeOasisDexOrder(manager, tradingAddress, {
+//     id: orderId,
+//     makerQuantity: enhancedOrder.sell,
+//     takerQuantity: enhancedOrder.buy,
+//     maker: enhancedOrder.owner,
+//   })
+// }
+
+// export var getHubs = async () => {
+//   var manager = await getManagerFromLocalStorage();
+//   return await Protocol.managersToHubs(manager, manager.deployment.melonContracts.version, manager.wallet.address)
+// }
+
+// export var getRoutes = async () => {
+//   var manager = await getManagerFromLocalStorage();
+//   return await Protocol.managersToRoutes(manager, manager.deployment.melonContracts.version, manager.wallet.address)
+// }
+
+// var removeDuplicateOrders = (orders) => orders.reduce((collectedOrders, order) => {
+//   var quantities = collectedOrders.map(o => String(o.trade.base.quantity))
+//   if (quantities.indexOf(String(order.trade.base.quantity)) < 0) collectedOrders.push(order)
+//   return collectedOrders;
+// }, new Array());
