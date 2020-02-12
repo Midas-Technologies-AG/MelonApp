8325c24d237744c6203e1b1cd11de0dfd1b62064
iemwill <willie@laubenheimer.eu>
2020-02-12 20:49:27 +0100

    typo for directory

--- Web/src/components/header.js
+++ Web/src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/header.css'
+import '../styles/header.css'
 import { getName, getInfo } from '../wrapper/melon'
 
 class Header extends React.Component {
